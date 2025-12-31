174cdca039729f4d82bd93a5d3e4012ab22bdafe
Gabriele <gmercolino2003@gmail.com>
2025-12-31 14:17:50 +0100

    feat: added utility method for loading sprite from a resource

--- src/main/java/com/github/gabrielemercolino/swingsprite/Sprite.java
+++ src/main/java/com/github/gabrielemercolino/swingsprite/Sprite.java
@@ -1,6 +1,23 @@
 package com.github.gabrielemercolino.swingsprite;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import static java.lang.System.err;
 
 public record Sprite(BufferedImage image) {
+	public static Sprite fromResource(final String name) {
+		URL resource = Sprite.class.getClassLoader().getResource(name);
+		if (resource == null) return null;
+
+		try {
+			return new Sprite(ImageIO.read(resource));
+		} catch (IOException e) {
+			err.println("Error while reading image");
+			e.printStackTrace(err);
+			return null;
+		}
+	}
 }
